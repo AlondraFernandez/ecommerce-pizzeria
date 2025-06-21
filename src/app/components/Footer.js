2147bf090c7ad27a8986d30d5ac4526a11740a9a
ALONDRA FERNANDEZ <alondra2017gaitan@gmail.com>
2025-06-21 16:55:36 -0300

    redes

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -5,15 +5,12 @@ const Footer = () => {
     return (
         <footer className="bg-gray-900 text-white text-center py-6 mt-10 rounded-lg">
             <div className="container mx-auto">
-                <p className="mb-4">Contacto: contacto@pizzeriadesabor.com</p>
+                <p className="mb-4">creado por Alo Fernandez</p>
                 <div className="flex justify-center space-x-4 mb-4">
-                    <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+                    <a href="https://www.facebook.com/jope.cervini" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
                         <FaFacebook size={24} />
                     </a>
-                    <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
-                        <FaTwitter size={24} />
-                    </a>
-                    <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+                    <a href="https://www.instagram.com/pizzeria_jope/" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
                         <FaInstagram size={24} />
                     </a>
                 </div>
